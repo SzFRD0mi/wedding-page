826231ac1ed4437e698464a5fa291c788cb6a460
Gőgh Dominik <szfrd0mi@gmail.com>
2024-03-07 10:54:56 +0100

    add features like video, countdown and basic content and styling

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-  
+  displayCountdown();
 });
+
+function displayCountdown() {
+  let countDownDisplay = document.querySelector("#countdown")
+
+  let countDownDate = new Date("September 20, 2025 15:00:00").getTime();
+
+  let x = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    countDownDisplay.innerHTML = "<span class='days'>" + days +  " <label>Days</label></span> <span class='hours'>" + hours + " <label>Hours</label></span> <span class='minutes'>"
+    + minutes + " <label>Minutes</label></span> <span class='seconds'>" + seconds + " <label>Seconds</label></span>";
+
+    if (distance < 0) {
+      clearInterval(x);
+      countDownDisplay.innerHTML = "Wedding started!";
+    }
+  }, 1000);
+}
